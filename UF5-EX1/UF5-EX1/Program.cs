8be6863b16bd3c158b206f52248eb5e18346ed1a
Weixin <weixin.xu.2018@gmail.com>
2020-12-28 13:18:12 +0100

    EX1 Acabado

---
+++ UF5-EX1/UF5-EX1/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace UF5_EX1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Ejercicio 1
+            Calculos calculo = new Calculos();
+            calculo.inicio();
+
+
+        }
+    }
+}
