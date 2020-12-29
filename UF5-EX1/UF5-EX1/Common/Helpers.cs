af30c0dc6d4b8b05cabb60bdac78dae5407e7b2b
Weixin <weixin.xu.2018@gmail.com>
2020-12-29 19:06:52 +0100

    Refactor
    Refactor para usar misma funcion de numero primo

---
+++ UF5-EX1/UF5-EX1/Common/Helpers.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace UF5_EX1
+{
+    public static class Helpers
+    {
+        public static bool isPrime(int val) 
+        {
+            for (int i = 2; i < val; i++)
+                if (val % i == 0) return true;
+            return false;
+        }
+    }
+}
--- UF5-EX1/UF5-EX1/EX3/Primo.cs
+++ UF5-EX1/UF5-EX1/EX3/Primo.cs
@@ -10,12 +10,7 @@ namespace UF5_EX1
         {
             Console.WriteLine("Introduce un numero para ver si es primo o no:");
             int num = Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine($"El numero {(esPrimo(num) ? "no " : "")}es primo");
-        }
-        public bool esPrimo(int num) {
-            for (int i = 2; i < num; i++)
-                if (num % i == 0) return true;
-            return false;
+            Console.WriteLine($"El numero {(Helpers.isPrime(num) ? "no " : "")}es primo");
         }
     }
 }
